d557b500f6fe68959380f35f034948ad7ba2fa7b
SUPERNOOB20 <andycia802@gmail.com>
2026-07-26 17:57:34 -0300

    v1.0.2: Added 1d obstacles (previous commit) and slippery ice (current commit). Next version: animations...

---
+++ examples/platforming (collisions)/Example 3/platformer_example.cpp
@@ -0,0 +1,445 @@
+// Credits to Mike Shah for the boilerplating  -  https://youtu.be/yZl9X47cHi8
+// g++ platformer_example.cpp -O3 -o platformer_demo `pkg-config --libs --cflags sdl3`
+
+// .
+// .
+// .
+
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Adjust these to your liking! :3
+int floaty_jump_intensity = 10;            // The bigger, the longer you can hold the jump button for. Change to 0 if you want the player to always jump at the same height!
+float gravity = 1.0f;                     //  The bigger, the smaller your jumps.
+float ice_speed = 2.5f;
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+#include "platformer_example.h"
+
+static Uint64 frame = 0;
+
+static int jumping_frame_limit = floaty_jump_intensity;
+
+static int frames_since_started_jumping = -1;
+static bool player_just_jumped = (frames_since_started_jumping != -1);
+
+bool is_airborne = false;       // (!is_airborne) means is_on_the_ground.
+
+static bool player_can_jump = (frames_since_started_jumping >= 0) && (!is_airborne);
+
+
+
+const bool* keyboardState = SDL_GetKeyboardState(nullptr);   // Reminder for self: We use NULL in C, and we use nullptr in C++
+
+
+void update_jump_status(){
+    // jumping_frame_limit = floaty_jump_intensity;     // Uncommentate if your game varies floaty jump intensities on runtime.
+
+    player_just_jumped = (frames_since_started_jumping != -1);
+
+    player_can_jump = (frames_since_started_jumping != -1) || (!is_airborne);
+}
+
+
+
+
+
+
+void reset_vertical_timer(){
+    
+    if (player_just_jumped){
+        frames_since_started_jumping++;
+    }
+
+    if (frames_since_started_jumping > floaty_jump_intensity){
+        frames_since_started_jumping = -1;      // Now you can't hold your jump button forever and fly away anymore *chuckles*.
+    }
+}
+
+
+void reset_jump(){
+    if (!is_airborne){
+        frames_since_started_jumping = 0;       // You will always be able to jump if you're on the ground.
+    }
+}
+
+
+
+bool player_is_on_the_ground(supermotor::Rect current_obstacle, bool check_for_ice){
+
+    bool res = false;
+    bool standing_on_ice = false;
+
+    supermotor::Rect dummy_current_obstacle(current_obstacle);
+    dummy_current_obstacle.move_y(-2);  // Remember that our origin (0, 0) is the top-left corner of the screen.
+
+    if (((supermotor::collidingVertices(Player, current_obstacle)).size() == 0) && (supermotor::collidingVertices(Player, dummy_current_obstacle).size() > 0)) {
+        res = true;
+        standing_on_ice = true;
+    }    
+
+    if(check_for_ice && standing_on_ice) { horizontal_velocity *= ice_speed; } 
+
+    return res;
+}
+
+
+// consumes     std::vector<SDL_FRect> your_floor_obstacles_here
+// and          std::array <int, 2> current_player_pos = {0, 0};
+//
+// Should be pretty self explanatory:
+// If there are no current collisions, then the player is airborne.
+bool check_airborne(){
+
+    bool res = true;
+    
+    Uint32 your_floor_obstacles_amount = your_floor_obstacles_here.size();
+
+    for (int i = 0; i < your_floor_obstacles_amount; i++){
+
+        supermotor::Rect current_floor_obstacle = your_floor_obstacles_here[i];
+
+        if (player_is_on_the_ground(current_floor_obstacle, false)) {       // your_obstacles_here[i] is the current_obstacle.
+            // SDL_Log("is airborne: false");
+            res = false;
+        }
+    }
+    
+    return res;
+}
+
+
+
+// Apply slippery ice effect if player is above ice.
+void check_airborne_ice() {
+    Uint32 your_ice_obstacles_amount = your_ice_obstacles_here.size();
+
+    for (int i = 0; i < your_ice_obstacles_amount; i++){
+
+        supermotor::Rect current_ice_obstacle = your_ice_obstacles_here[i];
+
+        player_is_on_the_ground(current_ice_obstacle, true);
+    }
+}
+
+
+
+void vertical_velocity_decay() {
+    if (is_airborne){
+        vertical_velocity += gravity;
+    } else {
+        vertical_velocity = 0.0f;
+    }
+}
+
+
+
+
+// See https://wiki.libsdl.org/SDL3/BestKeyboardPractices and https://wiki.libsdl.org/SDL3/SDL_Scancode
+void process_vertical_movement(){
+
+
+    // Up arrow key.
+    if (keyboardState[SDL_SCANCODE_UP]) {   
+        if (player_can_jump){
+		    // Player jumps :3
+		    vertical_velocity = -10.0f;
+            frames_since_started_jumping++;
+        }
+    }
+}
+
+
+
+
+
+
+void process_horizontal_movement(){
+
+
+    // Left arrow key.
+    if (keyboardState[SDL_SCANCODE_LEFT]) {
+		// Player moves to the left :3
+		horizontal_velocity -= 3.0f;
+        if (keyboardState[SDL_SCANCODE_D]) {
+            horizontal_velocity -= 6.0f;
+        }
+    }
+
+
+    // Right arrow key.
+    if (keyboardState[SDL_SCANCODE_RIGHT]) {
+       	// Player moves to the right :3
+		horizontal_velocity += 3.0f;
+        if (keyboardState[SDL_SCANCODE_D]) {
+            horizontal_velocity += 6.0f;
+        }
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+void update_player_pos() {
+
+    // Stores player pos from the previous frame (for collision handling purposes).
+    Previous_player_pos = Current_player_pos;        // Previous_player_pos <--- Current_player_pos
+
+
+
+
+    // From here on... Current_player_pos <--- New_player_pos
+
+    process_vertical_movement();
+    process_horizontal_movement();
+
+    check_airborne_ice();
+
+    Current_player_pos.move_x(horizontal_velocity);
+    Current_player_pos.move_y(vertical_velocity);
+
+    horizontal_velocity = 0;
+    // vertical_velocity = 0;
+
+    vertical_velocity_decay();      // You can refactor this to use "float gravity" or "double gravity" as a parameter here if you want.
+
+    Current_player_pos = supermotor::handle_collisions(Previous_player_pos, Current_player_pos, your_floor_obstacles_here);
+
+    Player = supermotor::convert_supermotor_rect_to_sdl_rect(Current_player_pos, Player);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct SDL_Application{
+
+    SDL_Window* mWindow;
+    SDL_Renderer* mRenderer;
+    SDL_Texture* playerTexture;
+    SDL_Texture* iceTexture;
+    
+    bool running = true;
+
+    SDL_Surface* player_surface;
+    SDL_Surface* ice_surface;
+
+    // Constructor
+    SDL_Application(const char* title){
+	    SDL_Init(SDL_INIT_VIDEO);
+	    mWindow = SDL_CreateWindow(title, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE);    
+	    mRenderer = SDL_CreateRenderer(mWindow, nullptr);
+	    if (mRenderer == nullptr){
+		    assert (0 && "ERROR: Hardware acceleration not supported :c");
+	    } else {
+		    SDL_Log("Current renderer: %s", SDL_GetRendererName(mRenderer));
+		    SDL_Log("Available renderer drivers:");
+		    for (int i = 0; i > SDL_GetNumRenderDrivers(); i++) {
+			    SDL_Log("%d, %s", i + 1, SDL_GetRenderDriver(i));
+		    }
+            SDL_SetRenderLogicalPresentation(mRenderer, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_LOGICAL_PRESENTATION_LETTERBOX);
+	    }
+
+
+	    player_surface = SDL_LoadPNG("../Assets/silly_thing.png");
+	    if (player_surface == nullptr){
+		    assert (0 && "ERROR: Player file 'silly_thing.png' not found :c");
+	    }
+        playerTexture = SDL_CreateTextureFromSurface(mRenderer, player_surface);
+        SDL_DestroySurface(player_surface);
+
+
+        ice_surface = SDL_LoadBMP("../Assets/ice.bmp");
+	    if (player_surface == nullptr){
+		    assert (0 && "ERROR: Obstacle file 'ice.bmp' not found :c");
+	    }
+        iceTexture = SDL_CreateTextureFromSurface(mRenderer, ice_surface);
+        SDL_DestroySurface(ice_surface);
+        
+
+    }
+	// Destructor
+	~SDL_Application(){
+		SDL_Quit();
+	}
+
+
+	
+	void Input(){
+		SDL_Event event;
+
+		while (SDL_PollEvent(&event)){
+			if (event.type == SDL_EVENT_QUIT){
+				running = false;
+			} else if (event.type == SDL_EVENT_KEY_DOWN) {
+
+                // SDL_Log("CONGRATULA!!! You pressed the %d key :3)7", event.button.button);
+
+                if (event.button.button == 41){          // 41 is the escape key       (you can remap it if you want :3)
+                    SDL_Quit();     // Exits SDL.
+                    exit(0);       //  Taskkill.
+                }
+
+
+                /*
+                // Debug.
+                if (event.button.button == 9){          // 9 is the F key       (you can remap it if you want :3)
+                    
+                    SDL_Log("\n");
+
+                                        
+                    SDL_Log("Current frame: %ld", frame);
+                    SDL_Log("Player.x: %f", Player.x);
+                    SDL_Log("Player.y: %f", Player.y);
+                    SDL_Log("Player.w: %f", Player.w);
+                    SDL_Log("Player.h: %f", Player.h);
+                    
+                    SDL_Log("frames_since_started_jumping: %d", frames_since_started_jumping);
+                    SDL_Log("is airborne: %d", is_airborne);
+
+                    
+                    SDL_Log("Horizontal velocity: %f", horizontal_velocity);
+                    SDL_Log("Vertical velocity: %f", vertical_velocity);
+                }
+                */
+
+            }
+		}
+	}
+
+
+
+	void Update(){
+
+        is_airborne = check_airborne();
+
+        update_player_pos();
+
+        reset_vertical_timer();
+
+        update_jump_status();
+
+        reset_jump();
+	}
+
+    
+
+	void Render(){
+		SDL_SetRenderDrawColor(mRenderer, 0x00, 0x00, 0x00, 0xFF);            // Pro tip: Use SDL_SetRenderDrawColor with black (or don't use it at all) before SDL_RenderClear if you're on LETTERBOX logical presentation (which we are)
+		SDL_RenderClear(mRenderer);
+
+		SDL_SetRenderDrawColor(mRenderer, 0xBB, 0xAA, 0xEE, 0xFF);
+        SDL_RenderFillRect(mRenderer, &Background);
+
+
+        // SDL_SetRenderDrawColor(mRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+		SDL_RenderTexture(mRenderer, playerTexture, nullptr, &Player);                // Renders the player.
+
+        
+        SDL_SetRenderDrawColor(mRenderer, 0x00, 0x00, 0xFF, 0xFF);
+        Uint32 your_obstacles_amount = your_obstacles_here.size();
+        for (int i = 0; i < your_obstacles_amount; i++){
+    		SDL_RenderFillRect(mRenderer, &your_obstacles_here[i]);                           // Renders the obstacles.
+        }
+        
+
+        Uint32 your_ice_obstacles_amount = your_ice_obstacles_here.size();
+        for (int i = 0; i < your_ice_obstacles_amount; i++){
+    		SDL_RenderTexture(mRenderer, iceTexture, nullptr, &your_ice_obstacles_here[i]);                           // Renders the ice obstacles.
+        }
+
+		// draw other things here ...		
+
+
+		SDL_RenderPresent(mRenderer);
+	}
+
+    // Every tick is one iteration of the game loop.
+	void Tick(){
+
+        // SDL_Log("\n");
+        // SDL_Log("current_frame: %ld", frame);
+
+        /*
+        if (frame > 34){
+            SDL_Quit();
+            exit(0);
+        }
+        */
+
+		Input();
+		Update();
+		Render();
+
+        update_nudge();
+
+        // frame++;
+	}
+
+	void MainLoop(){
+		Uint64 fps = 0;
+		Uint64 lastTime = 0;
+		while(running){
+			Uint64 currentTick = SDL_GetTicks();
+			Tick();
+			fps++;
+
+			Uint64 deltaTime = SDL_GetTicks() - currentTick;
+            if (deltaTime < 16.6){
+                SDL_Delay(16.6 - deltaTime);
+            }
+			if (currentTick > lastTime + 1000) {
+				lastTime = currentTick;
+				std::string title;
+				title += "Current FPS: " + std::to_string(fps);
+				SDL_SetWindowTitle(mWindow, title.c_str());
+				fps = 0;
+			}
+		}
+	}
+};
+
+
+// Entry Point
+int main(int argc, char* argv[]){
+
+    your_obstacles_here.push_back(ObstacleFloor1);
+
+    your_obstacles_here.push_back(ObstacleWall1);
+    your_obstacles_here.push_back(ObstacleWall2);
+
+    your_floor_obstacles_here = your_obstacles_here;
+    your_floor_obstacles_here.push_back(ObstacleIce1);
+
+    your_ice_obstacles_here.push_back(ObstacleIce1);    
+
+    SDL_Log("Player.x: %f", Player.x);
+    SDL_Log("Player.y: %f", Player.y);
+    SDL_Log("Player.w: %f", Player.w);
+    SDL_Log("Player.h: %f", Player.h);
+
+	SDL_Application app("FPS test! Current FPS: ");
+	app.MainLoop();
+	return 0;
+}
+
---
+++ examples/platforming (collisions)/Example 3/platformer_example.h
@@ -0,0 +1,118 @@
+#ifndef PLATFORMER_EXAMPLE_HEADER_FILE
+#define PLATFORMER_EXAMPLE_HEADER_FILE
+
+
+
+// Usually you use header files to:
+// 1) Declare functions (declare function requirements/specification/arity/etc.) (GOOD habit)
+// or...
+// 2) As a lazy way to have global variables (BAD habit) 
+//
+// Of course, we are using this header file here for the purposes of 2) :3
+
+
+
+#include <SDL3/SDL.h>
+#include <string>
+#include <cassert>
+#include <vector>
+
+#include "../../../supermotor/essentials/collisions.h"
+#include "../../../supermotor/essentials/common.h"
+
+
+
+
+
+
+// std::vector<SDL_Texture*> your_textures_here;
+std::vector<SDL_FRect> your_obstacles_here;
+std::vector<SDL_FRect> your_floor_obstacles_here;
+std::vector<SDL_FRect> your_ice_obstacles_here;
+
+
+// Your player's texture resolution goes here.
+int player_texture_width = 45;
+int player_texture_height = 98;
+
+
+SDL_FRect Player{
+    .x = ((float) WINDOW_WIDTH)  / 2.0f,
+    .y = ((float) WINDOW_HEIGHT)  / 2.7f,
+    .w = ((float) (WINDOW_WIDTH) / (float) (WINDOW_HEIGHT)) * ((float) (player_texture_width)),          // WINDOW_WIDTH / WINDOW_HEIGHT = ratio
+    .h = ((float) (WINDOW_WIDTH) / (float) (WINDOW_HEIGHT)) * ((float) (player_texture_height))
+};
+
+
+SDL_FRect Background{
+    .x = 0.0f,
+    .y = 0.0f,
+    .w = WINDOW_WIDTH,
+    .h = WINDOW_HEIGHT
+};
+
+
+SDL_FRect ObstacleFloor1{
+    .x = 0,
+    .y = WINDOW_HEIGHT  / 1.4f,
+    .w = WINDOW_WIDTH,
+    .h = WINDOW_HEIGHT / 2
+};
+
+
+SDL_FRect ObstacleIce1{
+    .x = WINDOW_WIDTH  / 3.95f,
+    .y = WINDOW_HEIGHT  / 1.4f,
+    .w = WINDOW_WIDTH  / 2.0f,
+    .h = WINDOW_HEIGHT / 5.0f
+};
+
+
+
+SDL_FRect ObstacleWall1{
+    .x = 0,
+    .y = WINDOW_HEIGHT  / 3.0f,
+    .w = WINDOW_WIDTH / 10.0f,
+    .h = WINDOW_HEIGHT
+};
+
+
+SDL_FRect ObstacleWall2{
+    .x = WINDOW_WIDTH / 1.1f,
+    .y = WINDOW_HEIGHT  / 3.0f,
+    .w = WINDOW_WIDTH / 10.0f,
+    .h = WINDOW_HEIGHT
+};
+
+
+// std::array<int, 2> previous_player_pos = {0, 0};
+// std::array<int, 2> current_player_pos = {0, 0};
+
+SDL_FRect Dummy_previous_player_pos{
+    .x = WINDOW_WIDTH  / 2.0f,
+    .y = WINDOW_HEIGHT  / 2.7f,
+    .w = ((float) (WINDOW_WIDTH) / (float) (WINDOW_HEIGHT)) * ((float) (player_texture_width)),          // WINDOW_WIDTH / WINDOW_HEIGHT = ratio
+    .h = ((float) (WINDOW_WIDTH) / (float) (WINDOW_HEIGHT)) * ((float) (player_texture_height))
+};
+supermotor::Rect Previous_player_pos(Dummy_previous_player_pos);
+
+
+SDL_FRect Dummy_current_player_pos{
+    .x = WINDOW_WIDTH  / 2.0f,
+    .y = WINDOW_HEIGHT  / 2.7f,
+    .w = ((float) (WINDOW_WIDTH) / (float) (WINDOW_HEIGHT)) * ((float) (player_texture_width)),          // WINDOW_WIDTH / WINDOW_HEIGHT = ratio
+    .h = ((float) (WINDOW_WIDTH) / (float) (WINDOW_HEIGHT)) * ((float) (player_texture_height))
+};
+supermotor::Rect Current_player_pos(Dummy_current_player_pos);
+
+
+
+// Function declaration.
+// vertical_velocity_decay();
+// update_player_pos();
+// is_airborne();
+
+
+
+
+#endif
